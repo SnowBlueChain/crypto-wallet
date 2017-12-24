217cf0f48b67495f858fac7ed19a39d6a2eabd66
SnowBlueChain <frankwong661@gmail.com>
2017-12-24 18:05:44 +0100

    Add some administrator providers

---
+++ src/providers/administrator/log/log.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Log } from '../../../model/log';
+import { Response } from '../../../model/response';
+
+@Injectable()
+export class AdministratorLogProvider {
+
+  public static readonly allLogsPath: string = "cryptowallet/administrator/TOKEN/log";
+  public static readonly getLogPath: string = "cryptowallet/administrator/TOKEN/log/LOG_ID";
+
+  constructor(private http: HttpClient) {
+  }
+
+  public allLogs(token: string): Observable<Response<Array<Log>>> {
+    return this.http.get<Response<Array<Log>>>(AdministratorLogProvider.allLogsPath.replace("TOKEN", token));
+  }
+
+  public getLog(token: string, logId: number): Observable<Response<Log>> {
+    return this.http.get<Response<Log>>(AdministratorLogProvider.getLogPath.replace("TOKEN", token).replace("LOG_ID", logId.toString()));
+  }
+}
