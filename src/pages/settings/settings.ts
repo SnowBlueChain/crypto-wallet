5eec077083cb4b8a7a4156d26635f815a878abfd
SnowBlueChain <frankwong661@gmail.com>
2017-12-26 15:42:48 +0100

    Improve « Settings » page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Cryptocurrency } from '../../model/cryptocurrency';
+import { Setting } from '../../model/setting';
+import { RegisteredUserProvider } from '../../providers/registered/user/user';
 
-import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered/cryptocurrency/cryptocurrency';
+import { AuthenticationPage } from '../authentication/authentication';
 
 @Component({
   selector: 'page-settings',
@@ -11,25 +12,53 @@ import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered
 })
 export class SettingsPage {
 
-  allCryptocurrencies : Cryptocurrency[] = [];
+  public isRegistered: boolean = null;
+  public filteredSettings: Setting[] = [];
+  public allSettings: Setting[] = [];
 
-  filteredCryptocurrencies : Cryptocurrency[] = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public registeredUserProvider: RegisteredUserProvider) {
+    this.isRegistered = (window.localStorage.getItem("user") === "true");
+    if (!this.isRegistered) {
+      this.navCtrl.push(AuthenticationPage);
+    }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public unregisteredCryptocurrencyProvider: UnregisteredCryptocurrencyProvider) {
-    unregisteredCryptocurrencyProvider.allCryptocurrencies().subscribe(data => {
-      this.allCryptocurrencies = data.data;
-      this.filteredCryptocurrencies = data.data;
+    this.registeredUserProvider.allSettings(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allSettings = data.data;
+      this.filteredSettings = data.data;
     });
   }
 
-  public getCryptocurrencies(event: any): void {
-    this.filteredCryptocurrencies = this.allCryptocurrencies;
+  public onInsertSettingButtonClicked(): void {
+    console.warn("Insert setting button has been clicked");
+  }
+
+  public onRefreshSettingsButtonClicked(): void {
+    this.registeredUserProvider.allSettings(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allSettings = data.data;
+      this.filteredSettings = data.data;
+    });
+  }
+
+  public onFilterTriggered(event: any): void {
+    this.filteredSettings = this.allSettings;
 
     let filter = event.target.value;
     if (filter && filter.trim() != '') {
-      this.filteredCryptocurrencies = this.filteredCryptocurrencies.filter((cryptocurrency: Cryptocurrency) => {
-        return (cryptocurrency.name.toLowerCase().indexOf(filter.toLowerCase()) > -1) || (cryptocurrency.symbol.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+      this.filteredSettings = this.filteredSettings.filter((setting: Setting) => {
+        return setting.name.toLowerCase().indexOf(filter.toLowerCase()) > -1;
       });
     }
+  }
+
+  public onSettingOverviewButtonClicked(setting: Setting): void {
+    console.warn("Setting chart button has been clicked for the following setting: " + setting.name);
+  }
+
+  public onUpdateSettingButtonClicked(setting: Setting): void {
+    console.warn("Update setting button has been clicked for the following setting: " + setting.name);
+  }
+
+  public onDeleteSettingButtonClicked(setting: Setting): void {
+    console.warn("Delete setting button has been clicked for the following setting: " + setting.name);
   }
 }
