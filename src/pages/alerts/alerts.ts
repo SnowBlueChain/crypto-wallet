c2196c946aa353aa6c7866c0404f1c59c9395c24
SnowBlueChain <frankwong661@gmail.com>
2017-12-26 15:42:56 +0100

    Improve « Alerts » page

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Cryptocurrency } from '../../model/cryptocurrency';
+import { Alert } from '../../model/alert';
+import { RegisteredUserProvider } from '../../providers/registered/user/user';
 
-import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered/cryptocurrency/cryptocurrency';
+import { AuthenticationPage } from '../authentication/authentication';
 
 @Component({
   selector: 'page-alerts',
@@ -11,25 +12,53 @@ import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered
 })
 export class AlertsPage {
 
-  allCryptocurrencies : Cryptocurrency[] = [];
+  public isRegistered: boolean = null;
+  public filteredAlerts: Alert[] = [];
+  public allAlerts: Alert[] = [];
 
-  filteredCryptocurrencies : Cryptocurrency[] = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public registeredUserProvider: RegisteredUserProvider) {
+    this.isRegistered = (window.localStorage.getItem("user") === "true");
+    if (!this.isRegistered) {
+      this.navCtrl.push(AuthenticationPage);
+    }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public unregisteredCryptocurrencyProvider: UnregisteredCryptocurrencyProvider) {
-    unregisteredCryptocurrencyProvider.allCryptocurrencies().subscribe(data => {
-      this.allCryptocurrencies = data.data;
-      this.filteredCryptocurrencies = data.data;
+    this.registeredUserProvider.allAlerts(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allAlerts = data.data;
+      this.filteredAlerts = data.data;
     });
   }
 
-  public getCryptocurrencies(event: any): void {
-    this.filteredCryptocurrencies = this.allCryptocurrencies;
+  public onInsertAlertButtonClicked(): void {
+    console.warn("Insert alert button has been clicked");
+  }
+
+  public onRefreshAlertsButtonClicked(): void {
+    this.registeredUserProvider.allAlerts(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allAlerts = data.data;
+      this.filteredAlerts = data.data;
+    });
+  }
+
+  public onFilterTriggered(event: any): void {
+    this.filteredAlerts = this.allAlerts;
 
     let filter = event.target.value;
-    if (filter && filter.trim() != '') {
-      this.filteredCryptocurrencies = this.filteredCryptocurrencies.filter((cryptocurrency: Cryptocurrency) => {
-        return (cryptocurrency.name.toLowerCase().indexOf(filter.toLowerCase()) > -1) || (cryptocurrency.symbol.toLowerCase().indexOf(filter.toLowerCase()) > -1);
+    /*if (filter && filter.trim() != '') {
+      this.filteredAlerts = this.filteredAlerts.filter((alert: Alert) => {
+        return alert.name.toLowerCase().indexOf(filter.toLowerCase()) > -1;
       });
-    }
+    }*/
+  }
+
+  public onAlertOverviewButtonClicked(alert: Alert): void {
+    console.warn("Alert chart button has been clicked for the following alert: " + alert.id);
+  }
+
+  public onUpdateAlertButtonClicked(alert: Alert): void {
+    console.warn("Update alert button has been clicked for the following alert: " + alert.id);
+  }
+
+  public onDeleteAlertButtonClicked(alert: Alert): void {
+    console.warn("Delete alert button has been clicked for the following alert: " + alert.id);
   }
 }
