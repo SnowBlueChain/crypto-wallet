b93ded3f462de4f4048141cce711ffe20f9adf76
SnowBlueChain <frankwong661@gmail.com>
2017-12-26 12:32:02 +0100

    Improve « Favorites » page

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Cryptocurrency } from '../../model/cryptocurrency';
-
 import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered/cryptocurrency/cryptocurrency';
+import { RegisteredUserProvider } from '../../providers/registered/user/user';
 
 @Component({
   selector: 'page-favorites',
@@ -11,25 +11,52 @@ import { UnregisteredCryptocurrencyProvider } from '../../providers/unregistered
 })
 export class FavoritesPage {
 
-  allCryptocurrencies : Cryptocurrency[] = [];
+  public filteredFavoriteCryptocurrencies : Cryptocurrency[] = [];
 
-  filteredCryptocurrencies : Cryptocurrency[] = [];
+  private allFavoriteCryptocurrencies : Cryptocurrency[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public unregisteredCryptocurrencyProvider: UnregisteredCryptocurrencyProvider) {
-    unregisteredCryptocurrencyProvider.allCryptocurrencies().subscribe(data => {
-      this.allCryptocurrencies = data.data;
-      this.filteredCryptocurrencies = data.data;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public unregisteredCryptocurrencyProvider: UnregisteredCryptocurrencyProvider, public registeredUserProvider: RegisteredUserProvider) {
+    this.registeredUserProvider.allFavorites(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allFavoriteCryptocurrencies = data.data;
+      this.filteredFavoriteCryptocurrencies = data.data;
     });
   }
 
-  public getCryptocurrencies(event: any): void {
-    this.filteredCryptocurrencies = this.allCryptocurrencies;
+  public onRefreshButtonClicked(): void {
+    this.registeredUserProvider.allFavorites(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id"))).subscribe(data => {
+      this.allFavoriteCryptocurrencies = data.data;
+      this.filteredFavoriteCryptocurrencies = data.data;
+    });
+  }
+
+  public onFilterFieldUpdated(event: any): void {
+    this.filteredFavoriteCryptocurrencies = this.allFavoriteCryptocurrencies;
 
     let filter = event.target.value;
     if (filter && filter.trim() != '') {
-      this.filteredCryptocurrencies = this.filteredCryptocurrencies.filter((cryptocurrency: Cryptocurrency) => {
+      this.filteredFavoriteCryptocurrencies = this.filteredFavoriteCryptocurrencies.filter((cryptocurrency: Cryptocurrency) => {
         return (cryptocurrency.name.toLowerCase().indexOf(filter.toLowerCase()) > -1) || (cryptocurrency.symbol.toLowerCase().indexOf(filter.toLowerCase()) > -1);
       });
     }
+  }
+
+  public onAnalyticsButtonClicked(cryptocurrency: Cryptocurrency): void {
+    console.log("Analytics button has been clicked for the following cryptocurrency: " + cryptocurrency.symbol);
+  }
+
+  public onDeleteButtonClicked(cryptocurrency: Cryptocurrency): void {
+    this.registeredUserProvider.deleteFavorite(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), cryptocurrency).subscribe(data => {
+      console.log(data);
+
+      let filteredIndex: number = this.filteredFavoriteCryptocurrencies.indexOf(cryptocurrency);
+      if (filteredIndex != -1) {
+        this.filteredFavoriteCryptocurrencies.splice(filteredIndex, 1);
+      }
+
+      let allIndex: number = this.allFavoriteCryptocurrencies.indexOf(cryptocurrency);
+      if (allIndex != -1 && allIndex != filteredIndex) {
+        this.allFavoriteCryptocurrencies.splice(allIndex, 1);
+      }
+    });
   }
 }
