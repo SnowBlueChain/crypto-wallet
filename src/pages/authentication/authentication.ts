0fd03982ad59f8235843fccb1e409375c9a3ea88
SnowBlueChain <frankwong661@gmail.com>
2017-12-27 15:48:14 +0100

    Improve « Authentication » page

--- src/pages/authentication/authentication.ts
+++ src/pages/authentication/authentication.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
+
+import { UnregisteredUserProvider } from '../../providers/unregistered/user/user';
+import { TabsPage } from '../tabs/tabs';
 
 @Component({
   selector: 'page-authentication',
@@ -7,6 +11,30 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AuthenticationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public authenticationForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public unregisteredUserProvider: UnregisteredUserProvider) {
+    this.authenticationForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email, Validators.maxLength(250)])],
+      password: ['', Validators.compose([Validators.required, Validators.maxLength(250)])]
+    });
+  }
+
+  public onSubmit(value: any): void {
+    if (this.authenticationForm.valid) {
+      this.unregisteredUserProvider.authenticate(value.email, value.password).subscribe(data => {
+        console.warn(data);
+
+        window.localStorage.setItem("user", JSON.stringify(data.success));
+        window.localStorage.setItem("user.id", JSON.stringify(data.data.userId));
+        window.localStorage.setItem("user.email", value.email);
+        window.localStorage.setItem("user.password", value.password);
+        window.localStorage.setItem("user.token.id", JSON.stringify(data.data.id));
+        window.localStorage.setItem("user.token.value", data.data.value);
+        window.localStorage.setItem("user.token.endDate", JSON.stringify(data.data.endDate));
+
+        this.navCtrl.parent.select(0);
+      });
+    }
   }
 }
