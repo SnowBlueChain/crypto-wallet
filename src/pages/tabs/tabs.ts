9d818dee29b75934c56d2c7c9b149499e8ad068c
SnowBlueChain <frankwong661@gmail.com>
2017-12-27 15:48:20 +0100

    Improve « Tabs » page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -20,16 +20,6 @@ export class TabsPage {
   settingsRoot:any = SettingsPage;
 
   constructor(public navCtrl: NavController) {
-    /* TO REMOVE */
-    window.localStorage.setItem("user", "true");
-    window.localStorage.setItem("user.id", "1");
-    window.localStorage.setItem("user.lastname", "DELORME");
-    window.localStorage.setItem("user.firstname", "Loïc");
-    window.localStorage.setItem("user.email", "loic.delorme@test.fr");
-    window.localStorage.setItem("user.password", "loic.delorme");
-    window.localStorage.setItem("user.enabled", "true");
-    window.localStorage.setItem("user.administrator", "true");
-    window.localStorage.setItem("user.token.value", "dd99fa8c-4abe-47e3-944a-f6cbc51914a4");
-    /* TO REMOVE */
+    window.localStorage.clear();
   }
 }
