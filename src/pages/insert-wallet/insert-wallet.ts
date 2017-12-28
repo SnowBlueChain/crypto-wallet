005cf08f476089f5acfd598d34fb85a55b2fd431
SnowBlueChain <frankwong661@gmail.com>
2017-12-28 16:43:55 +0100

    Add « InsertWallet » page

--- src/pages/insert-wallet/insert-wallet.ts
+++ src/pages/insert-wallet/insert-wallet.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Wallet } from '../../model/wallet';
+import { WalletForm } from '../../forms/walletform';
 
 import { RegisteredUserProvider } from '../../providers/registered/user/user';
 
@@ -14,21 +15,20 @@ import { WalletsPage } from '../wallets/wallets';
 })
 export class InsertWalletPage {
 
-  public walletForm: FormGroup;
+  public walletForm: WalletForm;
+  public walletFormGroup: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public registeredUserProvider: RegisteredUserProvider) {
-    this.walletForm = formBuilder.group({
+    this.walletForm = new WalletForm();
+    this.walletFormGroup = formBuilder.group({
       name: ['', Validators.compose([Validators.required, Validators.maxLength(250)])]
     });
   }
 
   public onSubmit(value: any): void {
-    if (this.walletForm.valid) {
-      let wallet: Wallet = new Wallet();
-      wallet.name = value.name;
-      wallet.userId = parseInt(window.localStorage.getItem("user.id"));
-
-      this.registeredUserProvider.insertWallet(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), wallet).subscribe(data => {
+    if (this.walletFormGroup.valid) {
+      this.walletForm.userId = parseInt(window.localStorage.getItem("user.id"));
+      this.registeredUserProvider.insertWallet(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), this.walletForm).subscribe(data => {
         console.warn(data);
 
         this.navCtrl.setRoot(WalletsPage);
