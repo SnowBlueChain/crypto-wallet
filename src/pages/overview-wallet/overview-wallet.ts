e36e6ad411e968ffcef84614576d04127bb2c2a1
SnowBlueChain <frankwong661@gmail.com>
2017-12-28 16:31:13 +0100

    Add « OverviewWallet » page

--- src/pages/overview-wallet/overview-wallet.ts
+++ src/pages/overview-wallet/overview-wallet.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { Wallet } from '../../model/wallet';
+
+import { RegisteredUserProvider } from '../../providers/registered/user/user';
+
+import { UpdateWalletPage } from '../update-wallet/update-wallet';
+import { WalletsPage } from '../wallets/wallets';
 
 @Component({
   selector: 'page-overview-wallet',
@@ -11,7 +16,39 @@ export class OverviewWalletPage {
 
   public wallet: Wallet;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public registeredUserProvider: RegisteredUserProvider) {
     this.wallet = this.navParams.get("wallet");
+  }
+
+  public onUpdateWalletButtonClicked(): void {
+    this.navCtrl.push(UpdateWalletPage, { wallet: this.wallet });
+  }
+
+  public onDeleteWalletButtonClicked(): void {
+    let confirmationAlert = this.alertCtrl.create({
+      title: 'Are you sure?',
+      message: 'Do you really want to delete this wallet?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Ok',
+          role: null,
+          handler: () => {
+            this.registeredUserProvider.deleteWallet(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), this.wallet).subscribe(data => {
+              console.warn(data);
+
+              this.navCtrl.setRoot(WalletsPage);
+            });
+          }
+        }
+      ]
+    });
+
+    confirmationAlert.present();
   }
 }
