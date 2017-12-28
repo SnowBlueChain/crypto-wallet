e545848ca738be814e48228c5dd23c9d1aa34af1
SnowBlueChain <frankwong661@gmail.com>
2017-12-28 17:00:47 +0100

    Add « InsertSetting » page

--- src/pages/insert-setting/insert-setting.ts
+++ src/pages/insert-setting/insert-setting.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Setting } from '../../model/setting';
+import { SettingForm } from '../../forms/settingform';
 
 import { RegisteredUserProvider } from '../../providers/registered/user/user';
 
@@ -14,23 +14,21 @@ import { SettingsPage } from '../settings/settings';
 })
 export class InsertSettingPage {
 
-  public settingForm: FormGroup;
+  public settingForm: SettingForm;
+  public settingFormGroup: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public registeredUserProvider: RegisteredUserProvider) {
-    this.settingForm = formBuilder.group({
+    this.settingForm = new SettingForm();
+    this.settingFormGroup = formBuilder.group({
       name: ['', Validators.compose([Validators.required, Validators.maxLength(250)])],
       theme: ['', Validators.compose([Validators.required, Validators.maxLength(250)])]
     });
   }
 
   public onSubmit(value: any): void {
-    if (this.settingForm.valid) {
-      let setting: Setting = new Setting();
-      setting.name = value.name;
-      setting.theme = value.theme;
-      setting.userId = parseInt(window.localStorage.getItem("user.id"));
-
-      this.registeredUserProvider.insertSetting(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), setting).subscribe(data => {
+    if (this.settingFormGroup.valid) {
+      this.settingForm.userId = parseInt(window.localStorage.getItem("user.id"));
+      this.registeredUserProvider.insertSetting(window.localStorage.getItem("user.token.value"), parseInt(window.localStorage.getItem("user.id")), this.settingForm).subscribe(data => {
         console.warn(data);
 
         this.navCtrl.setRoot(SettingsPage);
