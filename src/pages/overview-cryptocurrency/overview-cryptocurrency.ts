253502e669eebfe66709b8c5f47dd0a81c2c7a0d
SnowBlueChain <frankwong661@gmail.com>
2017-12-29 16:33:06 +0100

    Add admin control

--- src/pages/overview-cryptocurrency/overview-cryptocurrency.ts
+++ src/pages/overview-cryptocurrency/overview-cryptocurrency.ts
@@ -11,10 +11,15 @@ import { UpdateCryptocurrencyPage } from '../update-cryptocurrency/update-crypto
 })
 export class OverviewCryptocurrencyPage {
 
+  public isAdministrator: boolean = null;
   public cryptocurrency: Cryptocurrency;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.cryptocurrency = this.navParams.get("cryptocurrency");
+  }
+
+  public ionViewWillEnter(): void {
+    this.isAdministrator = (window.localStorage.getItem("user.administrator") === "true");
   }
 
   public onUpdateCryptocurrencyButtonClicked(): void {
