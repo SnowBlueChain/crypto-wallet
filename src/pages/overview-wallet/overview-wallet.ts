f40f91eca91a51875d8d3b251b459faff3f9a4f2
SnowBlueChain <frankwong661@gmail.com>
2017-12-29 16:35:55 +0100

    Add assets overview into wallet overview

--- src/pages/overview-wallet/overview-wallet.ts
+++ src/pages/overview-wallet/overview-wallet.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Wallet } from '../../model/wallet';
 
 import { UpdateWalletPage } from '../update-wallet/update-wallet';
+import { AssetsPage } from '../assets/assets';
 
 @Component({
   selector: 'page-overview-wallet',
@@ -19,5 +20,9 @@ export class OverviewWalletPage {
 
   public onUpdateWalletButtonClicked(): void {
     this.navCtrl.push(UpdateWalletPage, { wallet: this.wallet });
+  }
+
+  public onOverviewAssetsButtonClicked(): void {
+    this.navCtrl.push(AssetsPage, { wallet: this.wallet });
   }
 }
