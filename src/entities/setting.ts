1d037992d0f12c2978c8c0dce4872a0c293d6d3d
SnowBlueChain <frankwong661@gmail.com>
2017-12-30 09:16:40 +0100

    Rename main folder

--- src/entities/setting.ts
+++ src/entities/setting.ts
@@ -3,6 +3,7 @@ export class Setting {
   public id: number;
   public name: string;
   public theme: string;
+  public chartPeriod: string;
   public creationDate: Date;
   public lastUpdate: Date;
   public userId: number;
