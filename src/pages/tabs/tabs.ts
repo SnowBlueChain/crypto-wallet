1ebac1b18f6376fcaa9a8a46ef9edf6e617b901e
SnowBlueChain <frankwong661@gmail.com>
2017-12-31 16:26:35 +0100

    Change into tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,7 +5,7 @@ import { HomePage } from '../home/home';
 import { AllFavoritesPage } from '../all-favorites/all-favorites';
 import { AllWalletsPage } from '../all-wallets/all-wallets';
 import { AllAlertsPage } from '../all-alerts/all-alerts';
-import { AllSettingsPage } from '../all-settings/all-settings';
+import { SettingsPage } from '../settings/settings';
 
 import { LocalInformationProvider } from '../../providers/local/information/information';
 
@@ -19,9 +19,8 @@ export class TabsPage {
   favoritesRoot:any = AllFavoritesPage;
   walletsRoot:any = AllWalletsPage;
   alertsRoot:any = AllAlertsPage;
-  settingsRoot:any = AllSettingsPage;
+  settingsRoot:any = SettingsPage;
 
   constructor(public navCtrl: NavController, public localInformationProvider: LocalInformationProvider) {
-    this.localInformationProvider.clearAllInformation(); // TO REMOVE
   }
 }
