1268129b881bef5981586c102f1298152ace74db
SnowBlueChain <frankwong661@gmail.com>
2018-01-04 19:17:13 +0100

    Improve CoinMarketCap provider

--- src/providers/coinmarketcap/price/price.ts
+++ src/providers/coinmarketcap/price/price.ts
@@ -8,10 +8,17 @@ import { CoinMarketCapResponse } from '../../../responses/coinmarketcapresponse'
 @Injectable()
 export class PriceCoinMarketCapProvider {
 
+  public static readonly allPricesPath: string = "coinmarketcap/RESOURCE_URL";
+  public static readonly allPricesBetweenPath: string = "coinmarketcap/RESOURCE_URL/START_DATE/END_DATE";
+
   constructor(private http: HttpClient) {
   }
 
   public allPrices(cryptocurrency: Cryptocurrency): Observable<CoinMarketCapResponse> {
-    return this.http.get<CoinMarketCapResponse>("/coinmarketcap/" + cryptocurrency.resourceUrl);
+    return this.http.get<CoinMarketCapResponse>(PriceCoinMarketCapProvider.allPricesPath.replace("RESOURCE_URL", cryptocurrency.resourceUrl));
+  }
+
+  public allPricesBetween(cryptocurrency: Cryptocurrency, startDate: string, endDate: string): Observable<CoinMarketCapResponse> {
+    return this.http.get<CoinMarketCapResponse>(PriceCoinMarketCapProvider.allPricesBetweenPath.replace("RESOURCE_URL", cryptocurrency.resourceUrl).replace("START_DATE", startDate).replace("END_DATE", endDate));
   }
 }
