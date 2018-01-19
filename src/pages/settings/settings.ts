0efac3413afea7ffbc2d83a7cf9f42fe89528c89
SnowBlueChain <frankwong661@gmail.com>
2018-01-19 11:36:46 +0100

    Add settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 
-import { RegisteredUserProvider } from '../../providers/registered/user/user';
-import { LocalInformationProvider } from '../../providers/local/information/information';
+import { RegisteredUserProvider } from '../../providers/registered/user';
+import { LocalStorageProvider } from '../../providers/storage/localstorage';
 
 import { UserAuthenticationPage } from '../user-authentication/user-authentication';
-import { AllSettingsPage } from '../all-settings/all-settings';
-import { AllTokensPage } from '../all-tokens/all-tokens';
-import { AllLogsPage } from '../all-logs/all-logs';
 import { UserOverviewPage } from '../user-overview/user-overview';
-import { HomePage } from '../home/home';
+import { AllTokensPage } from '../all-tokens/all-tokens';
+import { AllDevicesPage } from '../all-devices/all-devices';
+import { AllLogsPage } from '../all-logs/all-logs';
 import { AllCryptocurrenciesPage } from '../all-cryptocurrencies/all-cryptocurrencies';
+import { AllCurrenciesPage } from '../all-currencies/all-currencies';
 import { AllAlertTypesPage } from '../all-alerttypes/all-alerttypes';
+import { AllChartPeriodsPage } from '../all-chartperiods/all-chartperiods';
+import { AllThemesPage } from '../all-themes/all-themes';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-settings',
@@ -21,49 +24,74 @@ export class SettingsPage {
 
   public isAdministrator: boolean;
  
-  constructor(public navCtrl: NavController, public navParams: NavParams, public registeredUserProvider: RegisteredUserProvider, public localInformationProvider: LocalInformationProvider) {
-    this.isAdministrator = this.localInformationProvider.isUserAdministrator();
-  }
+  constructor(private navCtrl: NavController, private toastCtrl: ToastController, private registeredUserProvider: RegisteredUserProvider, private localStorageProvider: LocalStorageProvider) {}
 
   public ionViewWillEnter(): void {
-    if (!this.localInformationProvider.isUserRegistered()) {
+    if (!this.localStorageProvider.isUserRegistered()) {
       this.navCtrl.setRoot(UserAuthenticationPage, { onSuccessRedirect: SettingsPage });
-    } else {
-      this.isAdministrator = this.localInformationProvider.isUserAdministrator();
+      return;
     }
   }
 
-  public onOverviewSettingsButtonClicked(): void {
-    this.navCtrl.push(AllSettingsPage);
+  public ionViewDidEnter(): void {
+    if (!this.localStorageProvider.isUserRegistered()) {
+      return;
+    }
+
+    this.isAdministrator = this.localStorageProvider.isUserAdministrator();
+  }
+
+  public onOverviewAccountButtonClicked(): void {
+    this.navCtrl.push(UserOverviewPage, { user: this.localStorageProvider.getUser() });
+  }
+
+  public onLogOutButtonClicked(): void {
+    this.registeredUserProvider.deleteToken(this.localStorageProvider.getUserTokenValue(), this.localStorageProvider.getToken()).subscribe(result => {
+      this.localStorageProvider.clearAllInformation();
+
+      this.navCtrl.setRoot(HomePage);
+    }, error => {
+      console.error(error);
+
+      let toastOverlay = this.toastCtrl.create({
+        message: 'An error occured...',
+        duration: 3000,
+        position: 'top'
+      });
+
+      toastOverlay.present();
+    }); 
   }
 
   public onOverviewTokensButtonClicked(): void {
     this.navCtrl.push(AllTokensPage);
   }
 
+  public onOverviewDevicesButtonClicked(): void {
+    this.navCtrl.push(AllDevicesPage);
+  }
+
   public onOverviewLogsButtonClicked(): void {
     this.navCtrl.push(AllLogsPage);
-  }
-
-  public onOverviewAccountButtonClicked(): void {
-    this.navCtrl.push(UserOverviewPage, { user: this.localInformationProvider.getUser() });
-  }
-
-  public onLogOutButtonClicked(): void {
-    this.registeredUserProvider.deleteToken(this.localInformationProvider.getUserTokenValue(), this.localInformationProvider.getUserId(), this.localInformationProvider.getToken()).subscribe(data => {
-      console.warn(data);
-
-      this.localInformationProvider.clearAllInformation();
-
-      this.navCtrl.setRoot(HomePage);
-    }); 
   }
 
   public onOverviewCryptocurrenciesButtonClicked(): void {
     this.navCtrl.push(AllCryptocurrenciesPage);
   }
 
+  public onOverviewCurrenciesButtonClicked(): void {
+    this.navCtrl.push(AllCurrenciesPage);
+  }
+
   public onOverviewAlertTypesButtonClicked(): void {
     this.navCtrl.push(AllAlertTypesPage);
+  }
+
+  public onOverviewChartPeriodsButtonClicked(): void {
+    this.navCtrl.push(AllChartPeriodsPage);
+  }
+
+  public onOverviewThemesButtonClicked(): void {
+    this.navCtrl.push(AllThemesPage);
   }
 }
